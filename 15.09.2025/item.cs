ed2d7af898c7e24e9c34b7ba6ddde2e220b0dcc5
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2025-09-16 12:14:02 +0200

    Create item.cs

---
+++ 15.09.2025/item.cs
@@ -0,0 +1,15 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2
+{
+    public class Item
+    {
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
